e4ad4a4755df2acbed7ef41a1fee8a52a841821a
Louis <colletlouis@gmail.com>
2021-05-24 17:00:20 +0200

    Added cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const db = require('./config/db')
 require('dotenv').config()
 const routes = require('./routes/index')
+
+app.use(cors())
 
 app.use(express.json())
 app.use(
